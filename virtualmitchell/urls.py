686babb7c46067d3768c313e3177aa943eb477b0
Colin Waddell <mrcolin@gmail.com>
2018-07-08 19:53:05 +0100

    simpler mobile

--- virtualmitchell/urls.py
+++ virtualmitchell/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
     url(r'^', include('www.urls')),
 ]
