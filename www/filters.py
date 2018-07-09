14078191d4001deedecbc13640b186138929025e
Colin Waddell <mrcolin@gmail.com>
2018-07-09 22:34:35 +0100

    adding meta data

--- www/filters.py
+++ www/filters.py
@@ -14,6 +14,7 @@ class RecordFilter(django_filters.FilterSet):
         model = Record
         fields = {
             "tags": ["exact"],
+            "street": ["icontains"],
             "date": ['lt', 'gt'],
             "description": ["icontains"],
             "caption": ["icontains"],
