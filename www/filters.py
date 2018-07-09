7d5c1e812165b94c1e955807736b363e7af94e0a
Colin Waddell <mrcolin@gmail.com>
2018-07-09 12:11:30 +0100

    better title

--- www/filters.py
+++ www/filters.py
@@ -7,15 +7,12 @@ from data.models import Record, Tag
 class RecordFilter(django_filters.FilterSet):
 
     tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects)
-    date = django_filters.IsoDateTimeFilter()
 
     class Meta:
         model = Record
         fields = {
-            "street": ["icontains"],
-            "area": ["exact"],
+            "tags": ["exact"],
             "date": ['lt', 'gt'],
             "description": ["icontains"],
             "caption": ["icontains"],
-            "tags": ["exact"]
         }
