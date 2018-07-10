ba01e12ab3e106f87c5c9de93c2bfae05b5981a2
Colin Waddell <mrcolin@gmail.com>
2018-07-10 18:12:16 +0100

    needed for key

--- data/models.py
+++ data/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    title = models.TextField(primary_key=True)
+    title = models.CharField(max_length=50, primary_key=True)
 
     class Meta:
         ordering = ['title']
