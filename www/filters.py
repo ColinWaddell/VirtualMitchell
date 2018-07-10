856ddf016db08e8ac228283db215babf7c87b674
Colin Waddell <mrcolin@gmail.com>
2018-07-10 18:04:58 +0100

    better date search

--- www/filters.py
+++ www/filters.py
@@ -3,6 +3,11 @@ from django.db import models
 import django_filters
 from data.models import Record, Tag
 
+STATUS_CHOICES = [
+    (year, year)
+    for year in range(1770, 2001, 10)
+]
+
 
 class RecordFilter(django_filters.FilterSet):
 
@@ -10,12 +15,22 @@ class RecordFilter(django_filters.FilterSet):
         queryset=Tag.objects
     )
 
+    date__year__lt = django_filters.ChoiceFilter(
+        choices=STATUS_CHOICES,
+        empty_label="Before the year..."
+    )
+
+    date__year__gt = django_filters.ChoiceFilter(
+        choices=STATUS_CHOICES,
+        empty_label="After the year..."
+    )
+
     class Meta:
         model = Record
         fields = {
             "tags": ["exact"],
             "street": ["icontains"],
-            "date": ['lt', 'gt'],
+            "date": ['year__lt', 'year__gt'],
             "description": ["icontains"],
             "caption": ["icontains"],
         }
