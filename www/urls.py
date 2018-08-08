8aa5e7b1e0ff52e5833d817f95bbbcd92328bf14
Colin Waddell <mrcolin@gmail.com>
2018-08-08 20:31:36 +0100

    dont need this view just now

--- www/urls.py
+++ www/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     url(r'^map/', views.MapView.as_view(), name='map'),
     url(r'^record/location/edit/(?P<id>[\w-]+)$', views.RecordLocationAppView.as_view(), name='recordlocation'),
     url(r'^record/edit/(?P<id>[\w-]+)$', views.RecordUpdate.as_view(), name='recordedit'),
-    url(r'^location/edit/(?P<place_id>[\w-]+)$', views.LocationUpdate.as_view(), name='locationedit'),
 ]
