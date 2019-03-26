7e26dc3e3647ea68ad9179434eddb99a38f9f0ae
Colin Waddell <mrcolin@gmail.com>
2019-03-26 17:47:49 +0000

    package updates

--- virtualmitchell/settings.py
+++ virtualmitchell/settings.py
@@ -125,6 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_PAGINATION_CLASS':  None,
     'PAGE_SIZE': 100
 }
 
